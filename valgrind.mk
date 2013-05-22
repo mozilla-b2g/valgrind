e85d1d69d160b44b0f1e68dc6f8389a092cc20fa
Kyle Machulis <kyle@nonpolynomial.com>
2013-05-22 11:29:03 -0700

    Bug 854517: Package include file for valgrind repo

---
+++ valgrind.mk
@@ -0,0 +1,19 @@
+PRODUCT_PACKAGES += \
+	libvex-arm-linux \
+	libcoregrind-arm-linux \
+	libreplacemalloc_toolpreload-arm-linux \
+	vgpreload_core-arm-linux \
+	memcheck-arm-linux \
+	vgpreload_memcheck-arm-linux \
+	cachegrind-arm-linux \
+	callgrind-arm-linux \
+	helgrind-arm-linux \
+	vgpreload_helgrind-arm-linux \
+	drd-arm-linux \
+	vgpreload_drd-arm-linux \
+	massif-arm-linux \
+	vgpreload_massif-arm-linux \
+	none-arm-linux \
+	valgrind \
+	default.supp \
+	$(NULL)
